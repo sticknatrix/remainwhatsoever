4142118298f977eb0dd31f6d366888f49168783e
sticknatrix <64150703+sticknatrix@users.noreply.github.com>
2024-10-11 18:58:44 +0200

    Add files via upload
    maybe a proper use is there

---
+++ diagram_smoothen_only_a_filter.c
@@ -0,0 +1,190 @@
+/*
+wurde ich automatisch oder bewußt geblockt?!? kompakter nochmal...
+war doch Bi Bam Bu?!
+bse hatte auch einfluß auf mandelbiopsien?!? - Google Suche
+einer von 30 Ägyptern Anfang 1920er Jahre heroinsüchtig?!? über 200 Morde in London wegen Heroinsucht in den 1920ern?!? - Google Suche
+Farbträger Flimmern Empfang Analog TV - Google Suche
+Kartoffelbrei mit mayonaise - Google Suche
+brauner Ring außen diesmal innen bei Pickel oder was auch immer für ein Insektenstich wo ich ausbrennen wollte aber nur leicht wurde es schon etwas heller wie außen der dunkle ring außen durchaus noch bräunlich?!? - Google Suche
+Zigarettenverbrennung hier[leicht!gibt Narben!] auch schon 4 Monate dunkler wenn auch violett oder blau rot?!? - Google Suche
+wollte insekteninfektion oder malpickel rausbrennen?!?
+meinte pigmentbräunung wegen hautkrebs geimpft und das wär dann nur noch blut in der haut?!? - Google Suche
+Verhornung wird nie dunkler als gestaute Bräunung wo nicht mehr rot oder blau rot in dem sinn?!? - Google Suche
+Insekten stechen häufiger in Leberflecken?!?scheinbar?!? - Google Suche
+hätte mit 9 oder mit 10 noch auch probleme gehabt zu sagen ob eine vor gestellte Uhr nach geht?!?denken lernen?!? - Google Suche
+neurotizismus feste Formen wenig Terrain negativ denken Introversion?!? - Google Suche
+mnemokreativität - Google Suche
+hockte früher "zum spaß" katzen übereinander oder aneinander?!?
+an uhrwerk rumbasteln um drum computer oder drum machine zu bauen haben?!? - Google Suche
+Calista Flockhart - Google Suche
+Welch ein Vorbild wieder?!?
+
+Linsen machen gut satt aber gekocht - Google Suche
+Linsen wachsen auch fast überall, aber giftig wenn Boden giftig?!?eher?!?
+gehört haben würde man schon man erinnerte sich nur oft nicht?!?bei der Informationsmenge?!? - Google Suche
+ist das übermüdung oder denkt man noch im schlaf oder scheinbaren schlafen?!? - Google Suche
+muß man sich bei mehrstimmigen Chorgesang konzentrieren?!? - Google Suche
+(also nicht selbe Melodie?!?)]
+Papa sagte wenn Einer im Chor falsch singt stimmt alles nicht mehr?!? - Google Suche
+
+eine Mischung aus gegenphasiger Löschung und Moiré Muster möglich bei Senkrechtdrehung?!? - Google Suche
+kleinen Kreis von Funktion nein in dem Fall Bitmap markieren und drehen Frequenz dieselbe?!? - Google Suche
+als würde man eine Periodische Funktion in Streifen wie geschreddert zufällig zusammenbauen, meist stimmt im Durchschnitt die Frequenz noch?
+gibt aber Löschmuster Wahrscheinlichkeit?
+
+Media Shop Cube an Zugvogel hängen und schauen wo Er wirklich hinfliegt das würde noch gut gehen knapp?!? - Google Suche
+
+bolzenschuss schlimmer als schächten?!?die meisten Tiere beißen die Kehle durch, außer ein paar Vögel mit Fallenlassen oder Zertrümmern, oder Fische stechen auch Beute teilweise?!?
+Markus Helfer erzählte einmal Rinder und Schweine werden durch stromschlag getötet?!? - Google Suche
+Sperrpatent - Google Suche
+Dinge schlechter und dümmer bauen wegen Marktmacht, dafür Sperrpatent gegen Andere?!?wer das Patenrecht hat oder erworben hat, darf auch verhindern die Anwendung?!?
+arb diet?!?
+Katze schleppt Junge ins Freie wegen Hitze?!?
+
+so wohl fühlen Sie sich meist in Tierheim nicht?!?
+waren das autistische stereotypien wie Andreas Dathering lachte oder sind das Fingerlockerungsübungen tippen gitarre finger überkreuzen?!?
+rettet dem Krankenhäusl'er?!?
+
+benjamin kommt 5 minuten zu spät benjamin verpaßt die stunde?!? - GS
+Vertrag von Doktor Rappen?!?
+
+benjamin kam nicht in den tagesrhythmus und nahm nimmt vivinox oder hypothetisch barbiturate[noch gefährlicher?!?] einfach um der "pädagogik" eins reinzuwürgen?!?
+ist das nötigung?!?
+
+wenn man mit 256 Farben schon die Palette optimieren kann mit Dithering zusammen so gut, sollte High Color auch ausreichen mit einigermaßen Abstimmung oder Anpassung?!?16 Bit?!?
+
+Papa hatte einen bestimmten Gelassenheitsspruch auf Postkarte an mich?!?
+
+hab das Gefühl Mama darf über mein körper verfügen wenn sie mir kein bier gibt?!?
+
+Jonas ärgert zum Spaß seine Mama und Sie wird sehr schnell grantig auch allein wegen Hund heißt der Hund Lucky?!?
+besser nicht vor Straße provozieren man paßt schnell aus versehen nicht auf?!?
+ 
+
+Aspergerler?!? Häusler?!?
+
+Sie haben mit größter Sorgfalt genau einen Stein angepeilt so ist man wenigstens vorgewarnt wie auf dem Etikett?!? - GS
+nein, es waren scheinbar drei in Glas diesmal?!?
+
+was lange zehrt wird endlich, alk?!?
+
+Google oft keine Urlaubsinfos bei Öffnungszeiten?!?
+wenn Süßes im Supermarkt auch schlechter als Süßes beim Bäcker viele Sorten, wenigstens kaum Probleme wenn einen Tag rumliegt?!?
+bei fast 30 Grad drinnen wie gestern da auch noch?!?
+
+Bücherspende an Deutschland nach Zusammenbruch wiederaufbau ICD 4 preisreduziertes M€ngelexmplar?!?
+
+Katzen mögen nicht, wenn wir rauchen?!?
+*/
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(void)
+{
+ unsigned int function[]={1,2,3,4,5,6,7,5,3,1,1,2,3,4,5,6,7,10,9,10,11,12,9,6,3,1,3,6,36,12,15,18,15,14,13,12,11,10,9,9,9,9,20,9,9,8,7,6,5,6,7,9,10,12};
+unsigned int function2[]={1,2,3,4,5,6,7,5,3,1,1,2,3,4,5,6,7,10,9,10,11,12,9,6,3,1,3,6,36,12,15,18,15,14,13,12,11,10,9,9,9,9,20,9,9,8,7,6,5,6,7,9,10,12};
+
+
+signed int diffqodiff[52];
+float avg2_slope[52];
+int n,n2;
+
+n=0;
+while(n<52)
+{
+ avg2_slope[n]=function[n]*100;
+
+ n++;
+}
+
+n=0;
+while(n<52)
+{
+ avg2_slope[n+3]=avg2_slope[n+2]=avg2_slope[n+1]=avg2_slope[n]=(avg2_slope[n+1]+avg2_slope[n+2]+avg2_slope[n+3])/4.0;	
+  n+=4;	
+}
+n=0;
+while(n<51)
+{
+ diffqodiff[n]=((function[n+1]*100)/function[n]) /((function[n]*100)/(avg2_slope[n]));
+ n++;
+	
+}
+
+n2=1000;
+int n3=0;
+while ( n2>0)
+{
+int maxdiffqodiff=100;
+maxdiffqodiff=100;
+n=0;
+int found=0;
+found=0;
+while ( maxdiffqodiff>0)
+{
+ n=0;
+ while ( (int)abs(diffqodiff[n])!=(int)maxdiffqodiff&& n<51)n++;
+ if ( (int)abs(diffqodiff[n])==(int)maxdiffqodiff) found=1, diffqodiff[n]-=(diffqodiff[n]>0 ? 1 : -1),function[n]=(function[n]+avg2_slope[n]/100)/2;//also minus 1?!?
+ maxdiffqodiff--;
+ int n4=0,n5=0;
+ n5=40;
+ /*
+ if(maxdiffqodiff%10==0)
+ {
+ while (n5>0)
+ {
+  printf("\n");
+  n4=0;
+  while (n4<51){
+  if ( function[n4]>=n5)printf("%c",'#'),fflush(stdout); else printf(" "),fflush(stdout);n4++;}
+  n5--;
+ }
+ */
+ 
+ //doppelzuweisung mit -= oder += also mehrfachzuweisung?!?!meine?!?
+ }
+  if ( found==0 )goto printstretch;
+ n2--;
+ n3++;
+}
+
+
+printstretch:
+
+n=0;
+float strecke=0,strecke2=0;
+while ( n<52)strecke+=function[n],strecke2+=function2[n],n++;
+ printf("%d",1001-n3);
+ 
+ float scale=strecke/strecke2;
+float floatn=0;
+floatn=0;
+n=0;
+while ( n<51)printf("%d %d %f\n",function[(int)floatn],function2[(int)floatn],avg2_slope[n]),n++;
+ int n5,n4;
+ float floatn4;
+ n5=40;
+ while (n5>0)
+ {
+  printf("\n");
+  floatn4=0;
+  while (floatn4<51){
+  if ( function[(int)floatn4]>=n5)printf("%c",'#'),fflush(stdout); else printf(" "),fflush(stdout);floatn4+=scale;}
+  n5--;
+ }
+ 
+ n5=40;
+ while (n5>0)
+ {
+  printf("\n");
+  n4=0;
+  while (n4<51){
+  if ( function2[n4]>=n5)printf("%c",'#'),fflush(stdout); else printf(" "),fflush(stdout);n4++;}
+  n5--;
+ }
+ 
+ 
+ 
+
+}
